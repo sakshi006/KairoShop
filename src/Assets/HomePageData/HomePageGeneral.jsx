d498185657faf6d0ec33a657d8f07342321f58d1
Sakshi <>
2022-03-17 21:04:59 +0530

    feat: landing page added

---
+++ src/Assets/HomePageData/HomePageGeneral.jsx
@@ -0,0 +1,24 @@
+import { v4 as uuid } from "uuid";
+export const homePageGeneral = [
+  {
+    _id: uuid(),
+    img: "https://m.media-amazon.com/images/G/35/AudibleAU/en_AU/CRO/GMA/GMA_Acquisition_All_Prospects_HP_Icon_1_Mobile_AU_22-3-21_V2.png",
+    name: "Start your free trial",
+    description :"Sign up for a free 30-day trial. Your first audiobook is free, yours to keep even if you cancel.",
+  
+  },
+  {
+    _id: uuid(),
+    img: "https://m.media-amazon.com/images/G/35/AudibleAU/en_AU/CRO/GMA/GMA_Acquisition_All_Prospects_HP_Icon_3_Mobile_AU_6-4-21_v2.png",
+    name: "Listen anytime, anywhere",
+    description:"Hop on to the free Kairo shop to listen while cooking, doing chores, heading to work or before bed.",
+
+  },
+
+  {
+    _id: uuid(),
+    img: "https://m.media-amazon.com/images/G/35/AudibleAU/en_AU/CRO/GMA/GMA_Acquisition_All_Prospects_HP_Icon_2_Mobile_AU_6-4-21_V2.png",
+    name: "Entertainment for hours",
+   description:"Listen all you want to The Plus Catalogue—thousands of Kairo Originals, audiobooks and podcasts",
+  }
+]
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,13 +1,49 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
+import { homePageProducts } from "../Assets/HomePageData/Homepagedata";
+import { homePageGeneral } from "../Assets/HomePageData/HomePageGeneral";
+
+import "./Home.css";
+import headerImg1 from "../Assets/head2.jpg";
+import HorizontalCard from "../Components/HorizontalCard/HorizontalCard";
 
 export const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
+    <div className="content">
+      <div className="main-slider">
+        <img
+          src={headerImg1}
+          alt="main-slider"
+          className="hero-img responsive main-slider-img"
+        />
+        <div className="centered-head">
+          <h1 className="hero-text">Listen to audiobooks, podcasts, Kairo Originals and more</h1>
+        <Link to="/productlist">
+          <button className="show-products">
+            <strong>Show Products</strong>
+          </button>
+        </Link>
+          </div>
+        
+      </div>
+
+      <section className="bottom-section">
+        {homePageProducts.map((item)=>{
+          return(
+            <HorizontalCard key={item._id} item={item} />
+          )
+        })}
+      
+      </section>
+      <div className= "home-footer">
+      {homePageGeneral.map((item)=><>
+        <img src={item.img} alt="free-trial" />
+          <h3>{item.name}</h3>
+          <p>{item.description}</p>
+        </>)}
+      
+      </div>
     </div>
   );
 };
