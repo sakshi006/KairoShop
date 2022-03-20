f09041ca9a88049937327a2a9a28c2d80cd671a0
Sakshi <>
2022-03-20 12:19:19 +0530

    feat:productContext created

--- src/Pages/ProductList/ProductList.jsx
+++ src/Pages/ProductList/ProductList.jsx
@@ -3,26 +3,35 @@ import "./ProductList.css";
 import ProductCard from "../../Components/ProductCard/ProductCard";
 import Filter from "../../Components/Filter/Filter";
 import { useFilter } from "../../context/filterContext";
-import {sortProduct,categoryFilter,PriceFilter,BestSellerFunc} from "../../Utils"
+import {
+  sortProduct,
+  categoryFilter,
+  PriceFilter,
+  BestSellerFunc,
+} from "../../Utils";
 import { useProductContext } from "../../context/productContext";
-
 
 export const ProductList = () => {
   const {
-    state: { sortBy, category, price, BestSeller  },
+    state: { sortBy, category, price, BestSeller },
   } = useFilter();
-  const {products} = useProductContext();
+  const { products } = useProductContext();
 
-// console.log(products.length)
-  
+  // console.log(products.length)
+
   // const sortedProducts = sortProduct(products, sortBy);
   // const categoryFilteredProducts = categoryFilter(products,category)
   // const bookBestSeller = BestSellerFunc(categoryFilteredProducts, BestSeller)
   // const priceFilteredProducts = PriceFilter(bookBestSeller,price)
   // const sortedProductsFinal = sortProduct(priceFilteredProducts, sortBy);
 
-
-  const productsListFinal = sortProduct(PriceFilter(BestSellerFunc(categoryFilter([...products],category),BestSeller),price),sortBy)
+  const productsListFinal = sortProduct(
+    PriceFilter(
+      BestSellerFunc(categoryFilter([...products], category), BestSeller),
+      price
+    ),
+    sortBy
+  );
 
   return (
     <div className="  main-content-ecom">
@@ -30,7 +39,8 @@ export const ProductList = () => {
         <Filter />
         <div className="products-page">
           <div className="products-head">
-            Showing All Products <small>( {productsListFinal.length} Products)</small>
+            Showing All Products{" "}
+            <small>( {productsListFinal.length} Products)</small>
           </div>
           <div className="product-list">
             {productsListFinal.map((item) => {
