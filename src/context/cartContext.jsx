634b1ea103a6d8184c08174590ce0e70d33bdbac
Sakshi <>
2022-04-06 13:51:50 +0530

    fix: cart api

--- src/context/cartContext.jsx
+++ src/context/cartContext.jsx
@@ -1,4 +1,5 @@
-import { createContext, useContext, useReducer } from "react";
+import axios from "axios";
+import { createContext, useContext, useEffect, useReducer } from "react";
 import { cartReducerFunction } from "../reducer/cartReducer";
 
 const CartContext = createContext();
@@ -9,6 +10,23 @@ const CartContextProvider = ({ children }) => {
   const [cartProducts, setCartProducts] = useReducer(cartReducerFunction, {
     cartList: [],
   });
+
+  const getCartData = async () => {
+    setCartProducts({ type: "API_REQUEST" });
+    try {
+      const response = await axios.get("/api/user/cart");
+      setCartProducts({
+        type: "ADD_TO_CART",
+        payload: response.data.cart,
+      });
+    } catch (error) {
+      setCartProducts({ type: "ERROR_HANDLE" });
+    }
+  };
+
+  useEffect(() => {
+    getCartData();
+  }, []);
 
   return (
     <CartContext.Provider value={{ cartProducts, setCartProducts }}>
